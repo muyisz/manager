9292a86ec1ce9cf24a65168d93b6335fe6416c70
fooage <nullptr.wang@gmail.com>
2021-06-24 14:51:58 +0800

    Declaration defines the type

---
+++ system.cpp
@@ -0,0 +1,12 @@
+#include <map>
+#include <vector>
+
+#include "models.hpp"
+
+using namespace std;
+
+extern map<long long, Community> communities;
+extern map<long long, Action> actions;
+extern map<long long, Person> persons;
+
+Person *now;
