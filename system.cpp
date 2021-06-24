5eb1ec8630c7c62a480ff5df0a4444b6059ab9e0
fooage <nullptr.wang@gmail.com>
2021-06-24 21:25:44 +0800

    Complete some functions

--- system.cpp
+++ system.cpp
@@ -1,4 +1,5 @@
 #include <map>
+#include <string>
 #include <vector>
 
 #include "models.h"
@@ -11,42 +12,72 @@ extern map<long long, Person> persons;
 
 Person *now;
 
-bool checkUser(string id,string password)
-{
-
+bool checkUser(string id, string password) {
+  long long number = atoll(id.c_str());
+  if (persons.count(number) == 0) {
+    return false;
+  }
+  if (persons[number].GetPassword() == password) {
+    now = &persons[number];
+    return true;
+  } else {
+    return false;
+  }
 }
 
-void setUser(string name,string id,string password)
-{
-
+int setUser(string name, string id, string password) {
+  long long number = atoll(id.c_str());
+  if (persons.count(number) == 0) {
+    Person newPerson(atoll(id.c_str()), name, password);
+    persons[number] = newPerson;
+    now = &persons[number];
+    return 0;
+  } else {
+    return -1;
+  }
 }
 
- string getUserName(string id)
-{
+string getUserName() { return now->GetName(); }
 
+vector<Community> getJoinedClub() {
+  vector<Community> res;
+  for (auto it : now->joins) {
+    res.push_back(it.second);
+  }
+  return res;
 }
 
- vector<Community> getJoinedClub(string id)
-{
-
+vector<Community> getAllClub(string id) {
+  vector<Community> res;
+  for (auto it : communities) {
+    res.push_back(it.second);
+  }
+  return res;
 }
 
-vector<Community> getAllClub(string id)
-{
-
+vector<Action> getMyAction() {
+  vector<Action> res;
+  for (auto it : now->reviews) {
+    res.push_back(it.second);
+  }
+  return res;
 }
 
-vector<Action> getMyAction(string id)
-{
-
+int joinClub(string clubID) {
+  long long id = atoll(clubID.c_str());
+  if (communities.count(id) == 0) {
+    return -1;
+  } else {
+    communities[id].AppendMember();
+    now->JoinCommunity(&communities[id]);
+  }
 }
 
-int joinClub(string id,int clubId)
-{
-
-}
-
-int applyAction(string id,string actName,int clubId,string actDescription)
-{
-
+int applyAction(string actName, string time, string actDescription,
+                string clubID) {
+  long long communityID = atoll(clubID.c_str());
+  long long actID = actions.size() + 1;
+  actions[actID] = Action(actID, actName, time, actDescription, 0,
+                          communities[communityID], *now);
+  now->ApplyApplication(&actions[actID]);
 }
